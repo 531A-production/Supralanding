85ce90b82c9657193de75638383197ff1c5d9e23
531A-production <productprod7@gmail.com>
2025-11-22 15:51:12 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -125,9 +125,9 @@ export default function App() {
             </div>
             
             <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold text-white mb-8 tracking-tight">
-              Blockchain Made
+              Не просто вебсайти,
               <span className="block bg-gradient-to-r from-[#8B5CF6] via-[#A855F7] to-[#8B5CF6] bg-clip-text text-transparent text-glow-purple-strong animate-pulse-slow">
-                Simple & Accessible
+                а повноцінні системи продажів.
               </span>
             </h1>
             
